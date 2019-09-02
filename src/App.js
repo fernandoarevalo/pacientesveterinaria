0b5743a9c5d87418d9a762eef9131219dc6d7d2f
fernando.arevalo <fernando.arevalo@outsourcing.libertycolombia.com>
2019-09-02 09:50:54 -0500

    Form

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './bootstrap.min.css';
+import Header from './components/Header';
+import NuevaCita from './components/NuevaCita'
+
+class App extends Component {
+    render() {
+        return (
+            <div class="container">
+                <Header
+                    titulo='Administrador Pacientes Veterinaria'
+                />
+
+                <div className="row">
+                    <div className="col-md-10 mx-auto">
+                        <NuevaCita />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
